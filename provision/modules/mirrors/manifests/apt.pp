15c6373eaa50902c793b27254183152a66e8d8d2
SagaieNet <oscherrer@gmail.com>
2013-02-15 12:38:23 +0100

    Update provision/modules/mirrors/manifests/apt.pp

--- provision/modules/mirrors/manifests/apt.pp
+++ provision/modules/mirrors/manifests/apt.pp
@@ -17,17 +17,19 @@
 #
 class mirrors::apt {
 
-
-  exec { 'wget':
+  exec { 'wget_precise':
+    timeout => 3600, # This can take a while... Usually Not ;-)
     command     => '/usr/bin/wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb'
   }
 
-  exec { 'dpkg':
-    command     => '/usr/bin/dpkg -i puppetlabs-release-precise.deb'
+  exec { 'dpkg_precise':
+    command     => '/usr/bin/dpkg -i puppetlabs-release-precise.deb',
+    require => Exec["wget_precise"]
   }
 
   exec { 'apt_update':
-    command     => '/usr/bin/apt-get update'
+    command     => '/usr/bin/apt-get update',
+	  require => Exec["dpkg_precise"]
   }
 
 }
