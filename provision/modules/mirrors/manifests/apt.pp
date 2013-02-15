9e74be727ccf8d97fdc7b645d4b49d51a55ad445
SagaieNet <oscherrer@gmail.com>
2013-02-15 11:42:35 +0100

    Using wget and dpk to get puppetlabs repo.
    as per: Using the Puppet Labs Package Repositories
    http://docs.puppetlabs.com/guides/puppetlabs_package_repositories.html 

--- provision/modules/mirrors/manifests/apt.pp
+++ provision/modules/mirrors/manifests/apt.pp
@@ -19,25 +19,18 @@ class mirrors::apt {
 
   $puppetlabs_key = '4BD6EC30'
 
-  exec { 'apt_key_puppetlabs':
-    path    => '/bin:/usr/bin',
-    unless  => "/usr/bin/apt-key list | /bin/grep -q '${puppetlabs_key}'",
-    command => "apt-key adv --keyserver 'pgp.mit.edu' --recv-keys '${puppetlabs_key}'",
-    before  => File[ 'puppetlabs.list' ],
+  exec { 'wget':
+    command     => '/usr/bin/wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb',
+    refreshonly => true,
   }
 
-  file { 'puppetlabs.list':
-    ensure  => present,
-    path    => '/etc/apt/sources.list.d/puppetlabs.list',
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    content => template('mirrors/puppetlabs.list.erb'),
+  exec { 'dpkg':
+    command     => '/usr/bin/dpkg -i puppetlabs-release-precise.deb',
+    refreshonly => true,
   }
 
   exec { 'apt_update':
     command     => '/usr/bin/apt-get update',
-    subscribe   => File[ 'puppetlabs.list' ],
     refreshonly => true,
   }
 
