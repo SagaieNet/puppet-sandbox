b3a845a1daa0950c2396183c537592665f93850c
SagaieNet <oscherrer@gmail.com>
2013-02-15 12:08:31 +0100

    Update provision/modules/mirrors/manifests/apt.pp

--- provision/modules/mirrors/manifests/apt.pp
+++ provision/modules/mirrors/manifests/apt.pp
@@ -19,18 +19,15 @@ class mirrors::apt {
 
 
   exec { 'wget':
-    command     => '/usr/bin/wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb',
-    refreshonly => true,
+    command     => '/usr/bin/wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb'
   }
 
   exec { 'dpkg':
-    command     => '/usr/bin/dpkg -i puppetlabs-release-precise.deb',
-    refreshonly => true,
+    command     => '/usr/bin/dpkg -i puppetlabs-release-precise.deb'
   }
 
   exec { 'apt_update':
-    command     => '/usr/bin/apt-get update',
-    refreshonly => true,
+    command     => '/usr/bin/apt-get update'
   }
 
 }
